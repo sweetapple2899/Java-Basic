5ad1751f8e4fee1fbd0aa84faeaf9bc42b35ec0a
muhlisadiwiguna <muhlisadiwiguna@thames>
2019-02-26 22:55:19 +0700

    add hello world

--- Java-Basic/src/introduction/HelloWorld.java
+++ Java-Basic/src/introduction/HelloWorld.java
@@ -3,8 +3,9 @@ package introduction;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Hello World");
+		
 	}
 
 }
