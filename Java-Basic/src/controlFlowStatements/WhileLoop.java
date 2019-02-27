1aeadf18550166cee26b68135402899d8c2dc5dd
muhlisadiwiguna <muhlisadiwiguna@thames>
2019-02-27 22:07:32 +0700

    add while loop

---
+++ Java-Basic/src/controlFlowStatements/WhileLoop.java
@@ -0,0 +1,16 @@
+package controlFlowStatements;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+
+		int x = 0;
+		
+		while (x <= 5) {
+			System.out.println("Hello " + x);
+			x++;
+		}
+		
+	}
+
+}
