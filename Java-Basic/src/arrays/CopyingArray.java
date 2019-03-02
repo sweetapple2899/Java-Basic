d6604a753b064553a9bf86cf64bbe8b5df696cfe
muhlisadiwiguna <muhlisadiwiguna@thames>
2019-03-02 11:02:16 +0700

    add how to copy array

---
+++ Java-Basic/src/arrays/CopyingArray.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class CopyingArray {
+
+	public static void main(String[] args) {
+		char[] array1 = {'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd', '!'};
+		
+		// Copying array
+		char[] array2 = new char[5];
+		System.arraycopy(array1, 5, array2, 0, 5);
+		System.out.println(new String(array2)); // output is world
+		System.out.println();
+		
+		char[] array3 = new char[5];
+		System.arraycopy(array1, 0, array3, 0, 5);
+		System.out.println(new String(array3)); // output is hello
+		System.out.println();
+		
+		// Manipulating array
+		char[] array4 = java.util.Arrays.copyOfRange(array1, 0, 5);
+		System.out.println(new String(array4)); // output is hello
+		System.out.println();
+
+		char[] array5 = java.util.Arrays.copyOfRange(array1, 5, 10);
+		System.out.println(new String(array5)); // output is world
+
+	}
+
+}
