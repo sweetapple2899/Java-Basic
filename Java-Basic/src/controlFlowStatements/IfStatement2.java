cd59d261f92354fe8eda1483ba45cdbf222b21ee
muhlisadiwiguna <muhlisadiwiguna@thames>
2019-03-16 11:14:06 +0700

    changing code in class IfStatement2

--- Java-Basic/src/controlFlowStatements/IfStatement2.java
+++ Java-Basic/src/controlFlowStatements/IfStatement2.java
@@ -20,16 +20,16 @@ public class IfStatement2 {
 		System.out.printf("Sales per year \t\t\t\t: %,.2f (in million Rp) %n", sales);
 
 		if ((asset <= 50) && (sales <= 300)) {
-			System.out.println("Type \t\t\t\t\t: Mikro Company");
+			System.out.println("Classification \t\t\t\t: Mikro Company");
 			
 		} else if ((asset <= 500) && (sales <= 2500)) {
-			System.out.println("Type \t\t\t\t\t: Small Company");
+			System.out.println("Classification \t\t\t\t: Small Company");
 			
 		} else if ((asset <= 10000) && (sales <= 50000)) {
-			System.out.println("Type \t\t\t\t\t: Medium Company");
+			System.out.println("Classification \t\t\t\t: Medium Company");
 			
 		} else {
-			System.out.println("Type \t\t\t\t\t: Big Company");
+			System.out.println("Classification \t\t\t\t: Big Company");
 		}
 		
 	}
