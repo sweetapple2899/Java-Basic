c62df6301a10ef66110b29c3836cf782a2a94bfd
muhlisadiwiguna <muhlisadiwiguna@thames>
2019-03-16 11:06:33 +0700

    changing code in class IfStatement2

--- Java-Basic/src/controlFlowStatements/IfStatement2.java
+++ Java-Basic/src/controlFlowStatements/IfStatement2.java
@@ -13,20 +13,25 @@ public class IfStatement2 {
 		double asset = input.nextDouble();
 		
 		System.out.println("Please input sales value (in million Rp)");
-		
+
 		double sales = input.nextDouble();
 		
-		if (asset <= 50 && sales <= 300) {
-			System.out.println("Type \t\t: Mikro");
+		System.out.printf("Asset value without land & building \t: %,.2f (in million Rp) %n", asset);
+		System.out.printf("Sales per year \t\t\t\t: %,.2f (in million Rp) %n", sales);
+
+		if ((asset <= 50) && (sales <= 300)) {
+			System.out.println("Type \t\t\t\t\t: Mikro Company");
 			
-		} else if ((asset >= 500 && asset <= 5000) || (sales >= 300 && sales <= 2500)) {
-			System.out.println("Type \t\t: Small");
+		} else if ((asset <= 500) && (sales <= 2500)) {
+			System.out.println("Type \t\t\t\t\t: Small Company");
+			
+		} else if ((asset <= 10000) && (sales <= 50000)) {
+			System.out.println("Type \t\t\t\t\t: Medium Company");
+			
 		} else {
-			System.out.println("Type \t\t: Medium");
+			System.out.println("Type \t\t\t\t\t: Big Company");
 		}
 		
-		System.out.printf("Asset value \t: %,.2f (in million Rp) %n", asset);
-		System.out.printf("Sales per year \t: %,.2f (in million Rp) %n", sales);
 	}
 
 }
